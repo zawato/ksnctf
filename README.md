32a8417627d0267c75b5f387e42fc51561cbba96
zawato <stein.cytus@gmail.com>
2025-08-30 10:47:52 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ksnctf
+
+## Linuxコマンド
+file <ファイル名>
+ファイル形式を確認
+実行ファイル：BuildID[sha1]=
+
+objdump
+オブジェクトファイルの情報を表示
+-Sオプションを用いるとソースコードを含めた逆アセンブル情報を表示
+
+## Tips
+Chromeのデベロッパーツールで、consoleにペーストできない → 「allow pasting」と入力
---
+++ unya.py
@@ -0,0 +1,16 @@
+"""
+console.log()に入れてみる
+
+$(function(){$("form").submit(function(){var t=$('input[type="text"]').val();var p=Array(70,152,195,284,475,612,791,896,810,850,737,1332,1469,1120,1470,832,1785,2196,1520,1480,1449);var f=false;if(p.length==t.length){f=true;for(var i=0;i<p.length;i++)if(t.charCodeAt(i)*(i+1)!=p[i])f=false;if(f)alert("(」・ω・)」うー!(/・ω・)/にゃー!");}if(!f)alert("No");return false;});});
+"""
+def solve_flag():
+    p = [70, 152, 195, 284, 475, 612, 791, 896, 810, 850, 737, 1332, 1469, 1120, 1470, 832, 1785, 2196, 1520, 1480, 1449]
+    flag = ""
+    
+    for i in range(len(p)):
+        char_code = p[i] // (i + 1)
+        flag += chr(char_code)
+    
+    return flag
+
+print("フラグ:", solve_flag())
